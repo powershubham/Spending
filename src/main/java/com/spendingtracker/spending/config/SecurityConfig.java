34c2b4316dfbbdaee7b0aa336d8078deedddec02
powershubham <shub.sharma612@gmail.com>
2026-04-17 21:35:59 +0530

    added html permission

--- src/main/java/com/spendingtracker/spending/config/SecurityConfig.java
+++ src/main/java/com/spendingtracker/spending/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
 
                         .requestMatchers(
                                 "/",
-                                "/index.html",
+                                "/**/*.html",
                                 "/**/*.js",
                                 "/**/*.css",
                                 "/**/*.png",
